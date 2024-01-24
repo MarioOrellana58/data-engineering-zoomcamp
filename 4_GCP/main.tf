9fde1868fe1850d7c55c120f0fc6e50e85fd1692
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2024-01-23 22:53:00 -0600

    DE Zoomcamp 1.4.1 - Setting up the Environment on Google Cloud (Cloud VM + SSH access)

---
+++ 4_GCP/main.tf
@@ -0,0 +1,73 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project
+  region      = var.region
+  credentials = file(var.credentials)
+}
+
+resource "google_compute_instance" "de-zoomcamp" {
+  boot_disk {
+    auto_delete = true
+    device_name = "de-zoomcamp"
+
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20240110"
+      size  = 30
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-standard-4"
+  name         = "de-zoomcamp"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/thinking-glass-412101/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "182971974119-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "us-central1-a"
+}
---
+++ 4_GCP/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "Project"
+  default     = "thinking-glass-412101"
+}
+
+variable "region" {
+  description = "Project region"
+  default     = "us-central1"
+}
+
+variable "credentials" {
+  description = "Project credentials"
+  default     = "./keys/my-creds.json"
+}
